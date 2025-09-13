7d11a9501efecd2262a65da82dd9cc37b1c35886
Rob Shiels <rob@robshiels.com>
2025-09-13 14:34:07 +0100

    Fleshing out Gig and Promoter classes, working on Sale class

--- README.md
+++ README.md
@@ -6,3 +6,4 @@ Assignment 3 - Python module
 - Brainstorming initial ideas for assignment
 - Writing user stories and entities
 - Create models.py file with some basic classes
+- Created sandbox.ipynb to start experimenting with my classes
--- models.py
+++ models.py
@@ -1,33 +1,45 @@
+# As a promoter, I want to post upcoming gigs with details on artist, time, date, and venue, so fans will know what gigs are coming up.
+
+
+
 class Promoter:
 	def __init__(self, name):
-		self.name
+		self.name = name
 
 class Customer:
 	def __init__(self, name):
 		self.name = name
 
 class Gig:
-	def __init__(self, artist, description="", date, time, image_url="", venue):
+	def __init__(self, artist, description, date, time, image_url, venue, promoter):
 		self.artist = artist
 		self.description = description
 		self.date = date
 		self.time = time
 		self.image_url = image_url
 		self.venue = venue
+		self.promoter = promoter
 
 	def __repr__(self):
-		return f"Gig(artist='{self.artist}', date='{self.date}, venue='{self.venue}', status = '{self.status}')"
+		return f"Gig(artist='{self.artist}', description='{self.description}', date='{self.date} at {self.time}', venue='{self.venue}', promoter='{self.promoter}')"
 
-class Sale:
+
+
+
+class SaleStatus(Enum):
 	COMPLETE = object()
 	LIVE = object()
 	PENDING = object()
 
-	def __init__(self):
+
+class Sale:
+
+	def __init__(self, sale_date, sale_time, ticket_price):
 		self.sale_date = sale_date
 		self.sale_time = sale_time
-		self.status = Sale.PENDING
-		self.ticket_price = price
+		self.ticket_price = ticket_price
+		self.status = SaleStatus.PENDING
+		
 
 class Buy:
 	def __init__(self, promoter, buyer, quantity):
